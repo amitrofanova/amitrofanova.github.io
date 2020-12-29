dd9339e775574223a3eb5cc6399447b69cdd9514
alena <mittralena@gmail.com>
2020-12-29 17:56:48 +0500

    lint

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,35 +1,35 @@
-import $ from 'jquery';
+import $ from "jquery";
 
 function changeNav() {
-  if (document.documentElement.clientWidth > 767) {
-    $('.navbar').removeClass('navbar-default');
-    $('.navbar').removeClass('navbar-scrolled');
-    $('.navbar').addClass('navbar-fixed-top');
-    $('.navbar-brand').addClass('visible-xs');
-    if ($(this).scrollTop() > 0) {
-      $('.navbar').removeClass('navbar-transparent');
-      $('.navbar').addClass('navbar-white');
+    if (document.documentElement.clientWidth > 767) {
+        $(".navbar").removeClass("navbar-default");
+        $(".navbar").removeClass("navbar-scrolled");
+        $(".navbar").addClass("navbar-fixed-top");
+        $(".navbar-brand").addClass("visible-xs");
+        if ($(this).scrollTop() > 0) {
+            $(".navbar").removeClass("navbar-transparent");
+            $(".navbar").addClass("navbar-white");
+        } else {
+            $(".navbar").removeClass("navbar-white");
+            $(".navbar").addClass("navbar-transparent");
+        }
+        if ($(this).scrollTop() > $("#top").height()) {
+            document.getElementsByClassName("to-top")[0].style.display = "block";
+        } else {
+            document.getElementsByClassName("to-top")[0].style.display = "none";
+        }
+    } else if ($(this).scrollTop() > 0){
+        $(".navbar-brand").removeClass("visible-xs");
+        $(".navbar").addClass("navbar-scrolled");
+        $(".navbar").addClass("navbar-fixed-top");
+        $(".navbar-brand").addClass("hidden-xs");
+        document.getElementsByClassName("about")[0].style.paddingTop = "80px";
     } else {
-      $('.navbar').removeClass('navbar-white');
-      $('.navbar').addClass('navbar-transparent');
+        $(".navbar").removeClass("navbar-scrolled");
+        $(".navbar-brand").removeClass("hidden-xs");
+        $(".navbar-brand").addClass("visible-xs");
+        $(".navbar").addClass("navbar-default");
     }
-    if ($(this).scrollTop() > $('#top').height()) {
-      document.getElementsByClassName('to-top')[0].style.display = 'block';
-    } else {
-      document.getElementsByClassName('to-top')[0].style.display = 'none';
-    }
-  } else if ($(this).scrollTop() > 0) {
-    $('.navbar-brand').removeClass('visible-xs');
-    $('.navbar').addClass('navbar-scrolled');
-    $('.navbar').addClass('navbar-fixed-top');
-    $('.navbar-brand').addClass('hidden-xs');
-    document.getElementsByClassName('about')[0].style.paddingTop = '80px';
-  } else {
-    $('.navbar').removeClass('navbar-scrolled');
-    $('.navbar-brand').removeClass('hidden-xs');
-    $('.navbar-brand').addClass('visible-xs');
-    $('.navbar').addClass('navbar-default');
-  }
 }
 
 // function changeNavHeight() {
@@ -42,76 +42,76 @@ function changeNav() {
 // }
 
 function animateSkills() {
-  const topOffset = $('#skills_section').offset().top;
-  const bottomOffset = topOffset - $(window).height() + 150;
-  if ($(document).scrollTop() > bottomOffset) {
-    $.getJSON('assets/data/skills.json', (data) => {
-      $.each(data, (key, val) => {
-        $(document.getElementById(key)).animate({ width: `${val}%`, opacity: '1' }, 2000);
-      });
-    });
-  }
+    const topOffset = $("#skills_section").offset().top;
+    const bottomOffset = topOffset - $(window).height() + 150;
+    if ($(document).scrollTop() > bottomOffset) {
+        $.getJSON("assets/data/skills.json", (data) => {
+            $.each(data, (key, val) => {
+                $(document.getElementById(key)).animate({ width: `${val}%`, opacity: "1", }, 2000);
+            });
+        });
+    }
 }
 
 function openModal(modal) {
-  $(modal).show();
+    $(modal).show();
 }
 
 function closeModal() {
-  $(this).parent().hide();
+    $(this).parent().hide();
 }
 
 function showSlides(slidesContainer) {
-  const slides = slidesContainer.children('.slider__item');
-  let slideIndex = 0;
+    const slides = slidesContainer.children(".slider__item");
+    let slideIndex = 0;
 
-  function showCurrentSlide(n) {
-    for (let i = 0; i < slides.length; i += 1) {
-      slides[i].style.display = 'none';
+    function showCurrentSlide(n) {
+        for (let i = 0; i < slides.length; i += 1) {
+            slides[i].style.display = "none";
+        }
+        slides[n].style.display = "block";
     }
-    slides[n].style.display = 'block';
-  }
 
-  function showPreviousSlide() {
-    slideIndex -= 1;
-    if (slideIndex < 0) {
-      slideIndex = slides.length - 1;
+    function showPreviousSlide() {
+        slideIndex -= 1;
+        if (slideIndex < 0) {
+            slideIndex = slides.length - 1;
+        }
+        showCurrentSlide(slideIndex);
     }
-    showCurrentSlide(slideIndex);
-  }
 
-  function showNextSlide() {
-    slideIndex += 1;
-    if (slideIndex === slides.length) {
-      slideIndex = 0;
+    function showNextSlide() {
+        slideIndex += 1;
+        if (slideIndex === slides.length) {
+            slideIndex = 0;
+        }
+        showCurrentSlide(slideIndex);
     }
-    showCurrentSlide(slideIndex);
-  }
 
-  showCurrentSlide(0);
+    showCurrentSlide(0);
 
-  $('.prev').on('click', showPreviousSlide);
-  $('.next').on('click', showNextSlide);
+    $(".prev").on("click", showPreviousSlide);
+    $(".next").on("click", showNextSlide);
 }
 
 $(document).ready(() => {
-  changeNav();
-  animateSkills();
-  $(window).on('resize', changeNav);
-  $(window).on('scroll', changeNav);
-  $(window).on('scroll', animateSkills);
+    changeNav();
+    animateSkills();
+    $(window).on("resize", changeNav);
+    $(window).on("scroll", changeNav);
+    $(window).on("scroll", animateSkills);
 
-  $('.portfolio__hospital').on('click', () => {
-    openModal('#hospital');
-    const slidesContainer = $('.slider__hospital');
-    showSlides(slidesContainer);
-  });
+    $(".portfolio__hospital").on("click", () => {
+        openModal("#hospital");
+        const slidesContainer = $(".slider__hospital");
+        showSlides(slidesContainer);
+    });
 
-  $('.portfolio__recipes').on('click', () => {
-    openModal('#recipes');
-    const slidesContainer = $('.slider__recipes');
-    showSlides(slidesContainer);
-  });
+    $(".portfolio__recipes").on("click", () => {
+        openModal("#recipes");
+        const slidesContainer = $(".slider__recipes");
+        showSlides(slidesContainer);
+    });
 
-  $('.close').on('click', closeModal);
+    $(".close").on("click", closeModal);
 });
